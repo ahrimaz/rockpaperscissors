1d3d46a3e3f50f35b83e52f91674e93381b0dd6c
ahrimaz <eric.stanard@gmail.com>
2022-11-06 20:48:07 -0500

    added index.html, rockpaperscissors.js

---
+++ rockpaperscissors.js
@@ -0,0 +1,7 @@
+function playRound(playerSelection, computerSelection) {
+    // code goes here
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playeRound(playerSelection, computerSelection))
